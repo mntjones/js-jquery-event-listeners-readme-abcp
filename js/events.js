77549ac1b707b6cf86bb0393ede0e30dde332a62
Monica Tamaru Jones <mntamaru@gmail.com>
2018-01-21 19:47:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,7 +4,7 @@ function getIt(){
   }
   
   function frameIt(){
-    $("img").on("load", function(){$("img").addClass})
+    $("img").on("load", function(){$("img").addClass("tasty")})
   }
   
   function pressIt() {
